b35651437b75f0c1476733cce20fa4fad4d8492c
enoracrl <enora.corler@gmail.com>
2023-04-03 14:18:09 +0200

    adapting bambu

--- bin/run_bambu.r
+++ bin/run_bambu.r
@@ -18,6 +18,7 @@
 ################################################
 ################################################
 library(bambu)
+library(BSgenome)
 
 ################################################
 ################################################
@@ -34,11 +35,36 @@ Rsamtools::indexFa(genomeseq)
 annot_gtf      <- strsplit(grep('--annotation*', args, value = TRUE), split = '=')[[1]][[2]]
 readlist       <- args[5:length(args)]
 
+print("BAMs:")
+readlist
+
 ################################################
 ################################################
 ## RUN BAMBU                                  ##
 ################################################
 ################################################
 grlist <- prepareAnnotations(annot_gtf)
-se     <- bambu(reads = readlist, annotations = grlist, genome = genomeSequence, ncore = ncore, verbose = TRUE)
+se <- bambu(
+    reads = readlist,
+    annotations = grlist,
+    genome = genomeSequence,
+    ncore = ncore,
+    verbose = TRUE,
+    opt.discovery = list(
+        min.readCount = 5,
+        max.txNDR = 1,
+        min.txScore.singleExon = 0)
+    )
+
+# Extract NDR
+tx_infos   <- se@rowRanges@elementMetadata@listData
+new_tx_idx <- tx_infos[["newTxClass"]] != "annotation"
+write.csv(
+    data.frame(tx_infos[c("GENEID", "TXNAME", "txNDR")])[new_tx_idx,],
+    "bambu_ndr.csv",
+    quote = FALSE,
+    row.names = FALSE
+)
+
+# Write GTF and counts
 writeBambuOutput(se, output_tag)
